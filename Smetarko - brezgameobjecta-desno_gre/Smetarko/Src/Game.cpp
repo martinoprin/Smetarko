a0aaf82b1798ee8404c655627dc738b48ef523a8
martinoprin <martinoprin8@gmail.com>
2024-02-29 19:04:27 +0100

    initialized

---
+++ Smetarko - brezgameobjecta-desno_gre/Smetarko/Src/Game.cpp
@@ -0,0 +1,77 @@
+#include "Game.h"
+#include "TextureManager.h"
+
+SDL_Texture* playerTex;
+SDL_Rect* srcR, destR;
+
+Game::Game()
+{}
+
+Game::~Game()
+{}
+
+void Game::init(const char* title, int width, int height, bool fullscreen)
+{
+	int flags = 0;
+
+	if (fullscreen)
+	{
+		flags = SDL_WINDOW_FULLSCREEN;
+	}
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
+	{
+		window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, flags);
+		renderer = SDL_CreateRenderer(window, -1, 0);
+		if (renderer)
+		{
+			SDL_SetRenderDrawColor(renderer, 0, 176, 196, 255);
+		}
+
+		isRunning = true;
+	}
+
+	//SDL_Surface* tmpSurface = IMG_Load("assets/player.png");
+	//playerTex = SDL_CreateTextureFromSurface(renderer, tmpSurface);
+	//SDL_FreeSurface(tmpSurface);
+	playerTex = TextureManager::LoadTexture("assets/player.png", renderer);
+}
+
+void Game::handleEvents()
+{
+	SDL_Event event;
+
+	SDL_PollEvent(&event);
+
+	switch (event.type)
+	{
+	case SDL_QUIT:
+		isRunning = false;
+		break;
+	default:
+		break;
+	}
+}
+
+void Game::update()
+{
+	cnt++;
+	destR.h = 64;
+	destR.w = 64;
+	destR.x = cnt;
+	std::cout << cnt << std::endl;
+}
+
+void Game::render()
+{
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, playerTex, NULL, &destR);
+	SDL_RenderPresent(renderer);
+}
+
+void Game::clean()
+{
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
+}
